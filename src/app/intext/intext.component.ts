266d71a4eb94cbe7425b1680e3f3099bf96d213e
Chaminda Amarasinghe <chamindaamarasinghe@sidero.ie>
2020-02-16 18:32:54 +0000

    initial working for LZW compression

--- src/app/intext/intext.component.ts
+++ src/app/intext/intext.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-intext',
@@ -7,9 +10,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntextComponent implements OnInit {
 
-  constructor() { }
+  public textField = "";
+  public mainText = "";
+  public textL;
+  public textP;
+  public textC;
+  public textR;
+  public start = 0;
+  public len = 1;
+  public p = "";
+  public c = "";
+  public codestream = "";
+  public pos = -1;
+  public pStartPos = -1;
+  public dict = {};
+  public rdict = {};
+
+  public step = 0;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.initDirect();
   }
+
+  addSpace() {
+    //  this.initDirect();
+    this.mainText = "";
+    this.textField.split('').forEach(e => {
+      this.mainText = this.mainText + e + " ";
+    })
+    this.mainText = this.textField;
+    this.pos = -1;
+this.dict = {}
+this.codestream = "";
+  }
+
+  initDirect() {
+    this.dict = {}
+    let x: number = 1;
+    this.textField.split('').forEach(e => {
+      if (!(e in this.dict)) {
+        this.dict[e] = x++
+        this.rdict[x - 1] = e
+      }
+    })
+  }
+
+  onClickMe() {
+    if (this.pos == -1) {
+    this.initDirect()
+    this.pos = 0
+    this.c = this.mainText.charAt(this.pos);
+    return
+    }
+    if (this.pos == this.mainText.length) {
+      this.codestream = this.codestream + this.dict[this.p];
+      this.pos++
+    } else {
+      if (this.isInDict(this.p + this.c)) {
+        this.p = this.p + this.c;
+      } else {
+        this.codestream = this.codestream + this.dict[this.p];
+        let nextCode = Object.keys(this.dict).length + 1
+        this.dict[this.p + this.c] = nextCode
+        this.rdict[nextCode] = this.p + this.c
+        this.p = this.c
+        this.pStartPos = this.pos;
+      }
+      this.pos++
+      this.c = this.mainText.charAt(this.pos);
+    }
+
+  }
+
+ sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+  isInDict(t) {
+    console.log(t)
+    console.log(this.dict)
+    return t in this.dict
+  }
+
 
 }
