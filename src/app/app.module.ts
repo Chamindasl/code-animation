c03378598e7751a37388dafc07acdce02fa5d5c9
Chaminda Amarasinghe <chamindaamarasinghe@sidero.ie>
2020-02-23 22:53:56 +0000

    removed lzwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LzwtComponent } from './lzwt/lzwt.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { LzwdComponent } from './lzwd/lzwd.component';
 import { LzwcComponent } from './lzwc/lzwc.component';
+import { LzwdComponent } from './lzwd/lzwd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LzwtComponent,
-    LzwdComponent,
-    LzwcComponent
+    LzwcComponent,
+    LzwdComponent
   ],
   imports: [
     BrowserModule,
